dbce4d9b000b94d1072f7c456ad4deac1ef905d9
Meena Atai <2397995@cmontmorency.qc.ca>
2025-10-07 11:38:31 -0400

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <M5_PbHub.h>
+
+#define BROCHE_ATOM_FIL_BLANC 32 // connecté
+#define BROCHE_ATOM_FIL_JAUNE 26 //pixel pour key unit
+
+#define MA_BROCHE_BOUTON 39
+
+#define CANAL_KEY_UNIT 0
+
+M5_PbHub myPbHub;
+
+//CRGB keyPixel;
+
+CRGB pixelAtom;
+
+
+void setup() {
+  // Initialiser FastLED pour contrôler le pixel RGB du M5Stack Key Unit
+  //FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1);
+
+  // Initialiser FastLED pour contrôler le pixel RGB du M5Stack Atom Unit
+  FastLED.addLeds< WS2812, 27 , GRB >(&pixelAtom, 1);
+
+  // initialiser la touche du key unit 
+  //pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
+
+  // initialiser la touche du Atom unit 
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+
+  Wire.begin();
+  myPbHub.begin();
+  myPbHub.setPixelCount( CANAL_KEY_UNIT , 1);
+
+}
+
+void loop() {
+  //KEY
+  //int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
+  int maLectureKey = myPbHub.digitalRead( CANAL_KEY_UNIT );
+  
+  if ( maLectureKey == 1) { //couleur non appuyé
+    //keyPixel = CRGB(0, 0, 0); //noir
+    myPbHub.setPixelColor( CANAL_KEY_UNIT , 0 , 0, 0, 0 );
+   } else {
+    int rouge = random(0,256);
+    int vert = random(0,256);
+    //keyPixel = CRGB(rouge, vert, 0); //jaune
+    myPbHub.setPixelColor( CANAL_KEY_UNIT , 0 , rouge, vert, 0 );
+  }
+
+  // INTÉGRÉ - ATOM
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+
+  if ( maLectureBouton == 1) { //couleur non appuyé
+    pixelAtom = CRGB(0, 0, 0); //noir
+   } else {
+    pixelAtom = CRGB(255, 255, 0); //jaune
+  }
+
+  FastLED.show();
+
+  delay(20);
+}
+
